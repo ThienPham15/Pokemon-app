3aef1765cf130199b1f5a39a8275b036f1507cd3
Thien Pham <s2201227@edu.bc.fi>
2023-10-20 09:20:30 +0300

    add types to PokeCard

---
+++ main.js
@@ -0,0 +1,55 @@
+const content = document.querySelector('.pokeList');
+const searchName = document.querySelector('#searchBox'); 
+
+let pokeData = []
+
+async function fetchData() {await
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0')
+    .then(res => res.json()) //res as a variable named as res, could be respond
+    .then(data => {
+        const fetches = data.results.map(item => {
+            return fetch(item.url)
+            .then(res => res.json())
+            .then(data => {
+                return {
+                    id: data.id,
+                    name: data.name,
+                    img: data.sprites.other['official-artwork'].front_default,
+                    types: data.types,
+                };
+            });
+        });
+    Promise.all(fetches) //using when there is multiple fetches, fetch everything in order
+    .then(res => {
+        pokeData = res;
+        pokeCards();
+        console.log(pokeData);
+    });
+    });
+};
+
+/*A function adds things to html page*/
+function pokeCards(){
+    //map is a method to loop through each element in array and do sth(by function) with each element
+    const cards = pokeData.map(pokemon => {
+        console.log(pokemon.types);
+        return `<div class="pokeBox">
+        <div class="img"><img src=${pokemon.img}></div>
+        <div class="pokeName">${pokemon.name}</div>
+        <div class="pokeTypes">
+        ${pokemon.types.map((type) => getType(type)).join('')}
+        </div>
+        <div class="pokeRank">#${pokemon.id}</div>
+        </div>`
+    })
+    
+    content.innerHTML = cards 
+}
+    
+
+function getType(type) {
+    return `<p>${type.type.name}</p>`
+}
+
+fetchData(); 
+searchName.addEventListener('input',searchCards);
